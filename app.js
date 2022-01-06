5211b0c6852f20675ec40872e2342beff80f9b8a
smiethereens23nas <nicholas23smith@gmail.com>
2022-01-05 21:17:31 -0500

    Finished draft of the game, still needs single player function to work

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ let fireAttack = document.querySelector(".fire-img");
 
 //Animation that sends the fire ball img from the left to the right of the screen and hidding the water attack from Blastoise
 function charizardAnimationAtk() {
-  if (($(".fire-img")[0].style.marginLeft = "-775px")) {
+  if (($(".fire-img")[0].style.marginLeft = "-1400px")) {
     waterAttack.style.visibility = "hidden";
     fireAttack.style.visibility = "visible";
 
     $(".fire-img").animate({ marginLeft: "4000px" }, 1500);
   } else {
-    $(".fire-img")[0].style.marginLeft = "-775px";
+    $(".fire-img")[0].style.marginLeft = "-1400px";
   }
 }
 // let test = document.querySelector('.water-img')
@@ -71,7 +71,6 @@ function fireBallAtk() {
   }
   if (blastoiseHP === 0) {
     attackStats.innerHTML = "Blastoise has fainted. You have won the battle!";
-    hideButtons();
     hideButtons();
     charizardAnimationAtk();
   }
@@ -296,11 +295,107 @@ function hideButtons() {
   hydroCannon.style.visibility = "hidden";
   hydroPump.style.visibility = "hidden";
 }
+hideButtons();
 
-//animations
-// function charizardAnimationAtk() {
-//   $(".fire-ball").click(function () {
-//     $(".fire-img").animate({ "margin-left": "+=1400px", opacity: 0 }, "slow");
-//     $(".fire-img").animate({ "margin-left": "-=1400px", opacity: 0 }, "slow");
-//   });
-// }
+//show buttons function
+function showButtons() {
+  fireBall.style.visibility = "visible";
+  flamethrower.style.visibility = "visible";
+  dragonClaw.style.visibility = "visible";
+  overheat.style.visibility = "visible";
+  iceBeam.style.visibility = "visible";
+  waterGun.style.visibility = "visible";
+  hydroCannon.style.visibility = "visible";
+  hydroPump.style.visibility = "visible";
+}
+
+function showCharizardButtons() {
+  fireBall.style.visibility = "visible";
+  flamethrower.style.visibility = "visible";
+  dragonClaw.style.visibility = "visible";
+  overheat.style.visibility = "visible";
+}
+
+//animation to make the pokemon waver if hit by an attack
+
+//grab the single player and multiplayer buttons
+let singlePlayer = document.querySelector(".single-btn");
+let multiplayer = document.querySelector(".multi-btn");
+let choosePokemon = document.querySelector(".pokemon-choose");
+let charizardButton = document.querySelector(".charizard-btn");
+let blastoiseButton = document.querySelector(".blastoise-btn");
+let battleDetails = document.querySelector(".battle-details");
+let buttonContainer = document.querySelector(".btn-container");
+let playerButtons = document.querySelector(".player-btns");
+let battleText = document.querySelector(".battle-text");
+
+//Click Single player or Multiplayer
+function singlePlayerOption() {
+  singlePlayer.addEventListener("click", function () {
+    choosePokemon.style.display = "block";
+    charizardButton.style.display = "block";
+    blastoiseButton.style.display = "block";
+    multiplayer.style.display = "none";
+    singlePlayer.style.display = "none";
+  });
+}
+singlePlayerOption();
+
+function multiplayerOption() {
+  multiplayer.addEventListener("click", function () {
+    multiplayer.style.display = "none";
+    singlePlayer.style.display = "none";
+    playerButtons.style.display = "none";
+    battleText.style.marginTop = "250px";
+    battleDetails.style.visibility = "visible";
+    fireBall.style.visibility = "visible";
+    flamethrower.style.visibility = "visible";
+    dragonClaw.style.visibility = "visible";
+    overheat.style.visibility = "visible";
+    iceBeam.style.visibility = "visible";
+    waterGun.style.visibility = "visible";
+    hydroCannon.style.visibility = "visible";
+    hydroPump.style.visibility = "visible";
+  });
+}
+multiplayerOption();
+
+//Choose your pokemon and hide the opposite pokemon attack buttons
+function charizardIsChosen() {
+  charizardButton.addEventListener("click", function () {
+    fireBall.style.visibility = "visible";
+    flamethrower.style.visibility = "visible";
+    dragonClaw.style.visibility = "visible";
+    overheat.style.visibility = "visible";
+    choosePokemon.style.display = "none";
+    charizardButton.style.display = "none";
+    blastoiseButton.style.display = "none";
+    playerButtons.style.display = "none";
+    battleText.style.marginTop = "250px";
+    battleDetails.style.visibility = "visible";
+  });
+}
+charizardIsChosen();
+
+function blastoiseIsChosen() {
+  blastoiseButton.addEventListener("click", function () {
+    iceBeam.style.visibility = "visible";
+    waterGun.style.visibility = "visible";
+    hydroCannon.style.visibility = "visible";
+    hydroPump.style.visibility = "visible";
+    buttonContainer.style.display = "none";
+    choosePokemon.style.display = "none";
+    charizardButton.style.display = "none";
+    blastoiseButton.style.display = "none";
+    playerButtons.style.display = "none";
+    battleText.style.marginTop = "250px";
+    battleDetails.style.visibility = "visible";
+battleDetails.innerHTML = "You are " + "<span class='blast-span'>Blastoise</span>" + " facing off against " + "<span class='char-span'>Charizard</span>" + "."
+  });
+}
+blastoiseIsChosen();
+
+//function that has a random attack response after player 1 attacks, alternating turns
+
+
+//reset game function
